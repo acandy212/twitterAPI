133aecc7a2422fc37678fab018f9fbc8e61b4a4d
NyeishaJPace <nyeisha.j.pace@gmail.com>
2022-11-07 22:56:14 +0000

    added request

--- app.js
+++ app.js
@@ -1,13 +1,64 @@
-// This script extracts the ID of the authenticating user, so you do not have to pass it yourself.
-if (!pm.environment.get('access_token') || !pm.request.url.path.includes(':id')) {
-    return;
-}
+import express from "express";
+import { Client, auth } from "twitter-api-sdk";
+import dotenv from "dotenv";
+dotenv.config();
 
-const [userId] = pm.environment.get('access_token').split('-');
+const app = express();
 
-if (!userId) {
-    return;
-}
+const URL = process.env.URL || 'http://127.0.0.1';
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
-const userIdIndex = pm.request.url.path.indexOf(':id');
-pm.request.url.path[userIdIndex] = userId;
+
+const authClient = new auth.OAuth2User({
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  callback: `${URL}:${PORT}/callback`,
+  scopes: ["tweet.read","tweet.write","users.read"],
+});
+const client = new Client(authClient);
+  
+const STATE = "my-state";
+
+app.get("/callback", async function (req, res) {
+  try {
+    const { code, state } = req.query;
+    if (state !== STATE) return res.status(500).send("State isn't matching");
+    await authClient.requestAccessToken(code);
+    res.redirect("/tweets");
+  } catch (error) {
+    console.log(error);
+  }
+});
+  
+app.get("/login", async function (req, res) {
+  const authUrl = authClient.generateAuthURL({
+    state: STATE,
+    code_challenge_method: "s256",
+  });
+  res.redirect(authUrl);
+});
+
+app.get("/revoke", async function (req, res) {
+  try {
+    const response = await authClient.revokeAccessToken();
+    res.send(response);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/tweets", async function (req, res) {
+  try {
+    const response = await client.tweets.createTweet();
+  
+  console.log("response", JSON.stringify(response, null, 2));
+    res.send(response);
+  } catch (error) {
+    console.log("tweets error", error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Go here to login: ${URL}:${PORT}/login`);
+});
+  
