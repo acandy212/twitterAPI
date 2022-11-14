ec78b67c38359efb997379158759e6a48106e69d
farmermichael1 <farmer_michael1@columbusstate.edu>
2022-11-14 04:41:47 +0000

    Added auth.html, auth.js, manage.html, Edited app

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { Client, auth } from "twitter-api-sdk";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 dotenv.config();
 
 const apikey = 'YcywEuOacedK4NOFAw7xwo4oD'
@@ -8,6 +11,8 @@ const apiSecretKey = 'eqGsinvX8YNc9lLoJ2iC9Yjh7UCKxAz3r8pBFqHWnVpkguvWJo'
 const bearerToken = "AAAAAAAAAAAAAAAAAAAAANe0jAEAAAAA%2FQt5SwU4neqhuyQt448J7Zbyztg%3DWMzPu0ZYsrAOcBCKAvytspe8YWXerL9CpkXIuVx9ufzgMqcsVO"
 const accessToken = '1589724180580339713-JH5UCybpKlHBscYrxSESMMzRMPbknQ'
 const accessTokenSecret = 'u3klbTBhfN7PYrJf7LpaLgyZq3wNeO5yJcZ2IjerMH7f5'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // var T = new Twitter({
 //   consumer_key:       apikey,
@@ -22,53 +27,59 @@ const URL = process.env.URL || 'http://127.0.0.1';
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 
-const authClient = new auth.OAuth2User({
-  client_id: process.env.CLIENT_ID,
-  client_secret: process.env.CLIENT_SECRET,
-  callback: `${URL}:${PORT}/callback`,
-  scopes: ["tweet.read","tweet.write","users.read"],
-});
-const client = new Client(authClient);
+// const authClient = new auth.OAuth2User({
+//   client_id: process.env.CLIENT_ID,
+//   client_secret: process.env.CLIENT_SECRET,
+//   callback: `${URL}:${PORT}/callback`,
+//   scopes: ["tweet.read","tweet.write","users.read"],
+// });
+// const client = new Client(authClient);
   
-const STATE = "my-state";
+// const STATE = "my-state";
 
-app.get("/callback", async function (req, res) {
-  try {
-    const { code, state } = req.query;
-    if (state !== STATE) return res.status(500).send("State isn't matching");
-    await authClient.requestAccessToken(code);
-    res.redirect("/tweets");
-  } catch (error) {
-    console.log(error);
-  }
-});
+// app.get("/callback", async function (req, res) {
+//   try {
+//     const { code, state } = req.query;
+//     if (state !== STATE) return res.status(500).send("State isn't matching");
+//     await authClient.requestAccessToken(code);
+//     res.redirect("/tweets");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
   
-app.get("/login", async function (req, res) {
-  const authUrl = authClient.generateAuthURL({
-    state: STATE,
-    code_challenge_method: "s256",
-  });
-  res.redirect(authUrl);
-});
+// app.get("/login", async function (req, res) {
+//   const authUrl = authClient.generateAuthURL({
+//     state: STATE,
+//     code_challenge_method: "s256",
+//   });
+//   res.redirect(authUrl);
+// });
 
-app.get("/revoke", async function (req, res) {
-  try {
-    const response = await authClient.revokeAccessToken();
-    res.send(response);
-  } catch (error) {
-    console.log(error);
-  }
-});
+// app.get("/revoke", async function (req, res) {
+//   try {
+//     const response = await authClient.revokeAccessToken();
+//     res.send(response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
-app.get("/tweets", async function (req, res) {
-  try {
-    const response = await client.tweets.createTweet("test");
+// app.get("/tweets", async function (req, res) {
+//   try {
+//     const response = await client.tweets.createTweet("test");
   
-  console.log("response", JSON.stringify(response, null, 2));
-    res.send(response);
-  } catch (error) {
-    console.log("tweets error", error);
-  }
+//   console.log("response", JSON.stringify(response, null, 2));
+//     res.send(response);
+//   } catch (error) {
+//     console.log("tweets error", error);
+//   }
+// });
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/auth',function(req,res){
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname+'/auth.html'));
 });
 
 app.listen(3000, function() {
