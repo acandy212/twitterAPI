1d2a36be9defd1f53749ca0c12cc5666684e7631
farmermichael1 <farmer_michael1@columbusstate.edu>
2022-12-05 04:54:45 +0000

    testing

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ import path from "path";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import cors from 'cors';
+import os from 'os';
 dotenv.config();
 
-
+console.log("host name: " + os.hostname());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
